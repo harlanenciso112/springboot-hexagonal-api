5fb3e9ce09ef7276e34ef233228b43ee282c3c52
harlanenciso112 <harsanenciso@gmail.com>
2025-10-09 16:49:32 -0500

    First Commit

---
+++ src/main/java/com/services/ms/student/app/domain/model/Student.java
@@ -0,0 +1,18 @@
+package com.services.ms.student.app.domain.model;
+
+
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private Integer age;
+    private String address;
+}
