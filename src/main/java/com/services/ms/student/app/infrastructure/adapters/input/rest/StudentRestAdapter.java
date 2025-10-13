cf3a12f91d765c2d78ef2e462c6bac54bc16a2b7
harlanenciso112 <harsanenciso@gmail.com>
2025-10-12 22:03:35 -0500

    Rest Adapters

---
+++ src/main/java/com/services/ms/student/app/infrastructure/adapters/input/rest/StudentRestAdapter.java
@@ -0,0 +1,48 @@
+package com.services.ms.student.app.infrastructure.adapters.input.rest;
+
+import com.services.ms.student.app.application.ports.input.StudentServicePort;
+import com.services.ms.student.app.infrastructure.adapters.input.rest.mapper.StudentRestMapper;
+import com.services.ms.student.app.infrastructure.adapters.input.rest.model.request.StudentCreateRequest;
+import com.services.ms.student.app.infrastructure.adapters.input.rest.model.response.StudentResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/students")
+public class StudentRestAdapter {
+    private final StudentServicePort servicePort;
+    private final StudentRestMapper restMapper;
+
+    @GetMapping("/v1/api")
+    public List<StudentResponse> findAll(){
+        return restMapper.toStundentResponseList(servicePort.findAll());
+    }
+
+    @GetMapping("/v1/api/{id}")
+    public StudentResponse findAll(@PathVariable Long id){
+        return restMapper.toStudentResponse(servicePort.findById(id));
+    }
+
+    @PostMapping("/api/v1")
+    public ResponseEntity<StudentResponse> save(@Valid @RequestBody StudentCreateRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(restMapper.toStudentResponse(
+                        servicePort.save(restMapper.toStudent(request))));
+    }
+
+    @PutMapping("/api/v1/{id}")
+    public StudentResponse update(@PathVariable Long id, @Valid @RequestBody StudentCreateRequest request){
+        return restMapper.toStudentResponse(servicePort.update(id,restMapper.toStudent(request)));
+    }
+
+    @DeleteMapping("/v1/api/{id}")
+    public void delete(@PathVariable Long id){
+        servicePort.deleteById(id);
+    }
+}
